a8c242477f1a124664ac049624be8a798e76d6db
VasilyeuUV <vasilyeu.uv@mail.ru>
2021-12-08 01:50:04 +0300

    Example of use Stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -7,52 +7,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Flutter StatelessWidget Example',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const MyHomePage(title: 'Hello, world!'),
-      );
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() => _counter++);
-  }
-
-  @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-            ],
+        home: Scaffold(
+          backgroundColor: Colors.indigo,
+          appBar: AppBar(
+            title: const Text("StatelessWidget Example"),
+            centerTitle: true,
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _incrementCounter,
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
+          body: Center(
+            child: Container(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const <Widget>[
+                  LinearProgressIndicator(value: 23),
+                  Text('23 %',
+                      style: TextStyle(color: Colors.white, fontSize: 20)),
+                  Text('Press button to download',
+                      style: TextStyle(color: Colors.white, fontSize: 20)),
+                ],
+              ),
+            ),
+          ),
+          floatingActionButton: const FloatingActionButton(
+            onPressed: null,
+            child: Icon(Icons.cloud_download),
+          ),
         ),
       );
 }
