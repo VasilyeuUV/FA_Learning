55906756b4c68f4adfdfe8a7f9b033f97041cad2
VasilyeuUV <vasilyeu.uv@mail.ru>
2021-12-08 03:30:19 +0300

    Example of using the StatefullWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,34 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+/// Класс виджета, имеющего состояние
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _MyFirstAppState(); // класс состояния виджета
+  }
+}
+
+/// Класс состояния виджета
+class _MyFirstAppState extends State<MyApp> {
+  bool _loading =
+      false; // динамическая переменная - флаг загрузки (идёт или нет)
+  // late - инициализируем позже notNullable поле
+  late double _progressValue; // отображение процента загрузки файла
+
+  @override
+  void initState() {
+    // здесь задаются начальные значения для динамических переменных
+    _progressValue = 0.0;
+
+    super.initState(); // метод суперкласса (типа конструктор),
+    // вызывается один раз, когда объект вставляется в дерево виджетов
+  }
 
   @override
   Widget build(BuildContext context) => MaterialApp(
@@ -20,22 +45,50 @@ class MyApp extends StatelessWidget {
           body: Center(
             child: Container(
               padding: const EdgeInsets.all(16),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const <Widget>[
-                  LinearProgressIndicator(value: 23),
-                  Text('23 %',
+              child: _loading // если загрузка пошла, то отображаем процесс
+                  ? Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        LinearProgressIndicator(value: _progressValue),
+                        Text('${(_progressValue * 100).round()}%',
+                            style: const TextStyle(
+                                color: Colors.white, fontSize: 20)),
+                      ],
+                    )
+                  : const Text(
+                      'Press button to download', // иначе выводим текст
                       style: TextStyle(color: Colors.white, fontSize: 20)),
-                  Text('Press button to download',
-                      style: TextStyle(color: Colors.white, fontSize: 20)),
-                ],
-              ),
             ),
           ),
-          floatingActionButton: const FloatingActionButton(
-            onPressed: null,
-            child: Icon(Icons.cloud_download),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              // setState - уведомить Flutter об изменении state переменной (или объекта)
+              setState(() {
+                // при нажатии на кнопку изменяем флаг загрузки на противоположный
+                _loading = !_loading;
+                // имитируем загрузку в отдельном методе этого класса
+                _updateProgress();
+              });
+            },
+            child: const Icon(Icons.cloud_download),
           ),
         ),
       );
+
+  /// имитируем загрузку
+  void _updateProgress() {
+    // Пример таймера
+    const oneSec = Duration(seconds: 1);
+    Timer.periodic(oneSec, (Timer t) {
+      setState(() {
+        _progressValue += 0.2;
+        if (_progressValue > 1.0) {
+          _loading = false;
+          t.cancel();
+          _progressValue = 0.0;
+          return;
+        }
+      });
+    });
+  }
 }
