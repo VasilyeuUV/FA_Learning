0ac7e1e438385e4d8192031c303cd690fb39bd4a
VasilyeuUV <vasilyeu.uv@mail.ru>
2021-12-13 05:05:37 +0300

    Navigation2.0: end

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const BooksApp());
 
-/// Класс книги
+/// Модель книги
 class Book {
   final String title;
   final String author;
 
+  // конструктор
   Book(this.title, this.author);
 }
 
@@ -18,25 +19,129 @@ class BooksApp extends StatefulWidget {
   State<StatefulWidget> createState() => _BooksAppState();
 }
 
-///Состояние приложения
+/// Состояние приложения
 class _BooksAppState extends State<BooksApp> {
-  @override
-  void initState() {
-    super.initState();
-  }
+  /// Выбранная книга
+  Book? _selectedBook;
+
+  /// Список книг
+  List<Book> books = [
+    Book('Left Hand of Darkness', 'Ursula K. Le Guin'),
+    Book('Too Like the Lightning', 'Ada Palmer'),
+    Book('Kindred', 'Octavia E. Butler'),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Books App',
       home: Navigator(
-        pages: const [
+        // Шаблон страницы книги
+        pages: [
           MaterialPage(
-            key: ValueKey('BooksListPage'),
-            child: Scaffold(),
-          )
+            key: const ValueKey('BooksListPage'),
+            child: BooksListScreen(
+              books: books,
+              onTapped: _handleBookTapped,
+            ),
+          ),
+          if (_selectedBook != null) BookDetailsPage(book: _selectedBook)
         ],
-        onPopPage: (route, result) => route.didPop(result),
+
+        // вызывается при Navigation.pop для обновления состояния
+        onPopPage: (route, result) {
+          // проверка возможности перехода на новую страницу
+          if (!route.didPop(result)) {
+            return false;
+          }
+
+          // Update the list of pages by setting _selectedBook to null
+          setState(() {
+            _selectedBook = null;
+          });
+
+          return true;
+        },
+      ),
+    );
+  }
+
+  // Определяем выбранную книгу и запускаем обновление приложения
+  void _handleBookTapped(Book book) {
+    setState(() {
+      _selectedBook = book;
+    });
+  }
+}
+
+/// Шаблон информации о выбранной книге
+class BookDetailsPage extends Page {
+  final Book? book;
+
+  BookDetailsPage({
+    required this.book,
+  }) : super(key: ValueKey(book));
+
+  @override
+  Route createRoute(BuildContext context) {
+    return MaterialPageRoute(
+      settings: this,
+      builder: (BuildContext context) {
+        return BookDetailsScreen(book: book!);
+      },
+    );
+  }
+}
+
+class BooksListScreen extends StatelessWidget {
+  final List<Book> books;
+  final ValueChanged<Book> onTapped;
+
+  const BooksListScreen({
+    required this.books,
+    required this.onTapped,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView(
+        children: [
+          for (var book in books)
+            ListTile(
+              title: Text(book.title),
+              subtitle: Text(book.author),
+              onTap: () => onTapped(book),
+            )
+        ],
+      ),
+    );
+  }
+}
+
+class BookDetailsScreen extends StatelessWidget {
+  final Book book;
+
+  const BookDetailsScreen({
+    required this.book,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (book != null) ...[
+              Text(book.title, style: Theme.of(context).textTheme.headline6),
+              Text(book.author, style: Theme.of(context).textTheme.subtitle1),
+            ],
+          ],
+        ),
       ),
     );
   }
